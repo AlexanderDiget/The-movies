b38f5d86ab3f5481028c14e0ba02ac2cd4f2e5c4
Alexander Diget <alex40@live.dk>
2024-02-01 10:35:35 +0100

    Location class

--- TheMovies/Models/Location.cs
+++ TheMovies/Models/Location.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace TheMovies.Models
 {
-    class Location
+    public class Location
     {
-    }
+        private string cinema {  get; set; }
+        private string city { get; set; }
+	}
 }
